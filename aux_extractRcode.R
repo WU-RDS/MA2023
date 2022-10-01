2ba1e1ee390765004e8ccb7dc9b0d9105df822dc
Daniel Winkler <daniel.winkler@s.wu.ac.at>
2022-10-01 14:41:39 +0200

    cleanup

---
+++ aux_extractRcode.R
@@ -0,0 +1,4 @@
+knitr::purl("02-basic_data_handling.Rmd", output="./Code/01-basic_data_handling.R")
+knitr::purl("03-data_import.Rmd", output="./Code/02-data_import.R")
+knitr::purl("04-basic_statistics.Rmd", output="./Code/03-basic_statistics.R")
+knitr::purl("05-visualization.Rmd", output="./Code/04-visualization.R")
